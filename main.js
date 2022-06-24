55899b2f8b4e1a406a5f58eb65d2ba69f10604fd
JorgeVanegasReyes <reyesjedu@gmail.com>
2022-06-24 08:26:09 -0500

    Add new design with Css

--- main.js
+++ main.js
@@ -108,4 +108,23 @@ const dateEl = document.getElementById('date-picker')
 const countdownEl = document.getElementById('countdown')
 const countdownElTitle = document.getElementById('countdown-title')
 const countdownBtn = document.getElementById('countdown-button')
-const timeElements = document.querySelectorAll('span')
+const timeElements = document.querySelectorAll('span2')
+
+const completeEl = document.getElementById('complete')
+const completeElInfo = document.getElementById('complete-info')
+const completeBtn = document.getElementById('complete-button')
+
+let countdownTitle=''
+let countdownDate=''
+let countdownValue=Date;
+let countdownActive;
+let savedCountdown;   //para el guardar en localstorage el title y date
+
+const second = 1000
+const minute= second * 60
+const hour = minute * 60
+const day = hour * 24
+
+// Set date input min. with Todays date
+const today = new Date().toISOString().split('T')[0]
+dateEl.setAttribute('min',today)
